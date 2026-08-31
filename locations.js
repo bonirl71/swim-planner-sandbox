82708c811382c090aade9703ed5b127a4cfe4bf0
bonirl71 <bonirl71@gmail.com>
2026-08-31 08:41:15 +0100

    Add swim locations data for lakes and seas

---
+++ locations.js
@@ -0,0 +1,99 @@
+const swimLocations = [
+
+    // =========================
+    // LAKE LOCATIONS
+    // =========================
+
+    {
+        id: 6,
+        name: "Youghal Quay",
+        county: "Tipperary",
+        latitude: 52.898509,
+        longitude: -8.310286,
+        distance: "TBC",
+        type: "lake"
+    },
+    {
+        id: 8,
+        name: "Portumna Harbour",
+        county: "Galway",
+        latitude: 53.083405,
+        longitude: -8.220102,
+        distance: "TBC",
+        type: "lake"
+    },
+    {
+        id: 9,
+        name: "Twomilegate",
+        county: "Clare",
+        latitude: 52.83623,
+        longitude: -8.46424,
+        distance: "TBC",
+        type: "lake"
+    },
+
+    // =========================
+    // SEA LOCATIONS
+    // =========================
+
+    {
+        id: 1,
+        name: "Blackhead Lighthouse and Gleninagh Pier",
+        county: "Clare",
+        latitude: 53.148,
+        longitude: -9.265,
+        distance: "2.5 km",
+        type: "sea",
+        tideStation: "IESHBWC070_0000_0100_MODELLED"
+    },
+    {
+        id: 2,
+        name: "Fanore Beach",
+        county: "Clare",
+        latitude: 53.125,
+        longitude: -9.290,
+        distance: "TBC",
+        type: "sea",
+        tideStation: "IESHBWC070_0000_0100_MODELLED"
+    },
+    {
+        id: 3,
+        name: "Lahinch Beach",
+        county: "Clare",
+        latitude: 53.053,
+        longitude: -9.344,
+        distance: "TBC",
+        type: "sea",
+        tideStation: "IESHBWC100_0000_0100_MODELLED"
+    },
+    {
+        id: 4,
+        name: "Kilkee Bay",
+        county: "Clare",
+        latitude: 52.679141,
+        longitude: -9.652973,
+        distance: "TBC",
+        type: "sea",
+        tideStation: "IESHBWC070_0000_0400_MODELLED"
+    },
+    {
+        id: 5,
+        name: "Whitestrand",
+        county: "Clare",
+        latitude: 52.747723,
+        longitude: -9.549059,
+        distance: "TBC",
+        type: "sea",
+        tideStation: "IESHBWC080_0000_0100_MODELLED"
+    },
+    {
+        id: 7,
+        name: "Lambay Island",
+        county: "Dublin",
+        latitude: 53.49222995430973,
+        longitude: -6.015908380729172,
+        distance: "TBC",
+        type: "sea",
+        tideStation: "IEEABWC020_0000_0200_MODELLED"
+    }
+];
